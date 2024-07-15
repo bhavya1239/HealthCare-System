2863ebed3d88000a548187c34cfe1e7ce75f166a
NitinS2001 <nitinjay2001@gmail.com>
2024-07-15 10:59:29 +0530

    Jwt Authentication removed

--- Backend/HealthCareSystem/src/main/java/com/healthcare/dto/User.java
+++ Backend/HealthCareSystem/src/main/java/com/healthcare/dto/User.java
@@ -2,27 +2,16 @@ package com.healthcare.dto;
 
 
 
-import com.healthcare.enumeration.Role;
 import jakarta.persistence.*;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import java.math.BigInteger;
 
-import java.util.Collection;
-import java.util.List;
 
 @Entity
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Table(name = "_user")
-public class User implements UserDetails {
+public class User {
     @Id
     @GeneratedValue//(strategy = GenerationType.UUID)
     private Integer id;
@@ -36,44 +25,5 @@ public class User implements UserDetails {
     private String password;
     private BigInteger contactNo;
     private String userRole;
-    //private String emailId;
-
-    @Enumerated(EnumType.STRING)
-    private Role role;
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(role.name()));
-    }
-
-    @Override
-    public String getPassword() {
-        return this.password;
-    }
-
-    @Override
-    public String getUsername() {
-        return this.email;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
 
 }
--- Backend/HealthCareSystem/src/main/java/com/healthcare/enumeration/Role.java
+++
@@ -1,6 +0,0 @@
-package com.healthcare.enumeration;
-
-public enum Role  {
-    USER,
-    ADMIN
-}
